e46d69202ddb5f04f5983987550189ec72eb8b30
kundu <kunduachyut19@gmail.com>
2024-04-19 20:56:23 +0530

    Final Commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,10 @@
 var weatherApi = "/weather";
 const weatherForm = document.querySelector("form");
-
 const search = document.querySelector("input");
-
 const weatherIcon = document.querySelector(".weatherIcon i");
-
 const weatherCondition = document.querySelector(".weatherCondition");
-
 const tempElement = document.querySelector(".temperature span");
-
 const locationElement = document.querySelector(".place");
-
 const dateElement = document.querySelector(".date");
 const currentDate = new Date();
 const options = { month: "long" };
